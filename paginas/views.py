bb01884876f5790af1ed7b7c12d94c43dfb1dd9e
Pedro Augusto Bragheto da Costa <47837910+Pedrxx@users.noreply.github.com>
2024-03-04 21:54:41 -0300

    Criação do Template

--- paginas/views.py
+++ paginas/views.py
@@ -5,3 +5,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
      template_name = 'paginas/index.html'
+     
+
+class SobreView(TemplateView):
+     template_name = 'paginas/sobre.html'
